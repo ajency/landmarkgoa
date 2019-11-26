a0ff30b3029e45278eacc4ef5a5e23595adfb467
latesh <latesh@ajency.in>
2019-11-26 12:44:41 +0530

    staging build

--- app/cart/src/components/config.js
+++ app/cart/src/components/config.js
@@ -14,11 +14,11 @@ let payU = {
 };
 
 let generalConfig = {
-    apiEndPoint : 'http://localhost:5000/project-ggb/asia-east-2/api/rest/v1',
+    // apiEndPoint : 'http://localhost:5000/project-ggb/asia-east-2/api/rest/v1',
 	// apiEndPoint : 'https://us-central1-project-ggb-dev.cloudfunctions.net/api/rest/v1',
-    // apiEndPoint:'https://asia-east2-project-ggb.cloudfunctions.net/api/rest/v1',
-    //webSiteLink:"http://greengrainbowl-com.digitaldwarve.staging.wpengine.com/"
-    webSiteLink:"https://greengrainbowl.com/",
+    apiEndPoint:'https://asia-east2-project-ggb.cloudfunctions.net/api/rest/v1',
+    webSiteLink:"http://greengrainbowl-com.digitaldwarve.staging.wpengine.com/",
+    // webSiteLink:"https://greengrainbowl.com/",
     company_name:"GGB",
     googleApiKey:'AIzaSyC-w19gW41OaoyjuK4jHPVN5JtviKGB7KQ'
     
