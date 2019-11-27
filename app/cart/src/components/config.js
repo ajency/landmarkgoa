d861a45f3507fb8442de50dfdda8f8e5c7d880f8
latesh <latesh@ajency.in>
2019-11-27 15:32:57 +0530

    added aj razorpay id

--- app/cart/src/components/config.js
+++ app/cart/src/components/config.js
@@ -1,5 +1,5 @@
 let razorPayConfig = {
-    api_key:'rzp_test_FSfmJofoQNnVG6',
+    api_key:'rzp_test_HZ5L6Z2Jm8bWco',
     customJs:"https://checkout.razorpay.com/v1/razorpay.js",
     standardJs:"https://checkout.razorpay.com/v1/checkout.js",
     image:"https://i.imgur.com/n5tjHFD.png",
